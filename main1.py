268c3ca015096262a84b671ef3ed7ccfbf12c946
sn9sivw <ilamokryakov@gmail.com>
2025-02-14 09:26:00 +0000

    Сделал

--- main1.py
+++ main1.py
@@ -2,12 +2,16 @@ from loguru import logger
 
 
 def main():
-    logger.add('file.log ',
-                format= '{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}',
+    logger.add('file.log',
+                format='{time:YYYY-MM-DD at HH:mm:ss} | {level} | {message}',
                 rotation="3 days",
                 backtrace=True,
                 diagnose=True)
 
+title = input("Введите название новеллы: ")
+url = input("Сделал ссылку на новеллу: ")
+logger.info (f"Пользователь ввел название {title} новеллы и ссылку {url}.")
 
-if __name__ == '__main':
+
+if __name__ == '__main__':
     main()
